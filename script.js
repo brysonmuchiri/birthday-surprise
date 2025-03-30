aba519782d46af6928f502ed6a8f5a08d3585dbe
brysonmuchiri <nyagabryson@gmail.com>
2025-03-30 16:14:48 -0700

    Initial commit: Happy +1 project

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => 
 const canvas =
 document.getElementById("fireworks");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+)
+console.log("%c",
+    "font-size: 200px; background:
+    url(https://github.com/brysonmuchiri/birthday-surprise/blob/main/Screenshot%202025-03-30%20132806.png?raw=true") no repeat;
+        background-size: cover; ");
+    )
+    
 const particles = [];
 
 function createFirework() {
@@ -13,7 +20,7 @@ function createFirework() {
     canvas.height / 2;
     let color = `hsl(${Math.random() *
         360}, 100%, 60%)`;
-        
+
         for (let i = 0; i < 30; i++) {
             particles.push( {
                 x: x,
@@ -26,6 +33,30 @@ function createFirework() {
             } ) ;
         }
 }
+
+function showBalloons() {
+    let container = document.getElementById("balloonContainer");
+    for (let i = 0; i < 10; i++) {  // Generate 10 balloons
+        balloon.src =
+        "https://github.com/brysonmuchiri/birthday-surprise/blob/main/Screenshot%202025-03-30%20153830.png?raw=true";
+        balloon.className = "balloon";
+        let balloon = document.createElement("img");
+        // Set random position
+        balloon.style.left = Math.random() * window.innerWidth + "px";
+        balloon.style.bottom = "-100px"; // Start below screen
+
+        // Random animation duration
+        let floatTime = Math.random() * 3 + 4; // Between 4-7 seconds
+        balloon.style.animation = `float ${floatTime}s linear forwards`;
+
+        container.appendChild(balloon);
+
+        // Remove balloon after animation
+        setTimeout(() => {
+            balloon.remove();
+        }, floatTime * 1000);
+        }
+    }
 
 function animateFireworks() {
     ctx.clearRect(0, 0, canvas.width,canvas.height);
@@ -46,9 +77,37 @@ function animateFireworks() {
                 i--;
             }
         }
-    
-        requestAnimationFrame(animateFireworks);
-    }
 
-    setInterval(createFirework, 700);
-    animateFireworks();
+        requestAnimationFrame(
+            animateFireworks);
+}
+
+setInterval(createFirework, 700);
+animateFireworks();
+
+{
+    const container = document.querySelector('.container');
+    const fireworks = new Fireworks.default(container, {
+      autoresize: true,
+      opacity: 0.5,
+      acceleration: 1.05,
+      friction: 0.97,
+      gravity: 1.5,
+      particles: 50,
+      trace: 3,
+      explosion: 5,
+      intensity: 30,
+      flickering: 50,
+      lineStyle: 'round',
+      hue: { min: 0, max: 360 },
+      delay: { min: 30, max: 60 },
+      rocketsPoint: 50,
+      lineWidth: { explosion: { min: 1, max: 3 }, trace: { min: 1, max: 2 } },
+      brightness: { min: 50, max: 80 },
+      decay: { min: 0.015, max: 0.03 },
+      mouse: { click: false, move: false, max: 1 },
+      sound: { enable: false }
+    });
+    fireworks.start();
+    console.log(document.querySelector("img").src);
+  });
